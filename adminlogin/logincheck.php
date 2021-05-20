964398f01769bcf880fc56ba1b2a584b30db53c3
bhagavathir <bhagavathir9@gmail.com>
2021-05-20 21:51:17 +0530

    the linking of sites

--- adminlogin/logincheck.php
+++ adminlogin/logincheck.php
@@ -3,19 +3,19 @@
     session_start();
     $connection = mysqli_connect('localhost','root');
     
-    $db = mysqli_select_db($connection,'phppractice');
+    $db = mysqli_select_db($connection,'placementportal');
     if(isset($_POST['submit'])){
         $user = $_POST['admin'];
         $pass = $_POST['password'];
         
-        $sql ="select * from admintable where adminUserId='$user' AND adminpass = '$pass'";
+        $sql ="select * from admintable where adminUserId = '$user' AND adminpass = '$pass'";
         $query = mysqli_query($connection,$sql);
         $row = mysqli_num_rows($query);
             
         if($row == 1){
                 echo "Admin Successfully logged in<br>";
                 $_SESSION['admin'] = $user;
-                header('Location:adminmainpage.php');
+                header('Location:dashboardpage/mainindex.php');
             }
             else{
                 echo "Log in failed<br>";
