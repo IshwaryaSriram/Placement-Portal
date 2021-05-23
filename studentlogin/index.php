060797a825797eb6d3d2e5ffe21ef3776591d759
kavyasridhar1501 <kavyasridhar1501@gmail.com>
2021-05-23 10:33:18 +0400

    chnage

--- studentlogin/index.php
+++ studentlogin/index.php
@@ -8,7 +8,7 @@
   if (isset($_GET['logout'])) {
   	session_destroy();
   	unset($_SESSION['username']);
-  	header("location: login.php");
+  	header("location: ../mainpage/index.html");
   }
 ?>
 <!DOCTYPE html>
@@ -38,7 +38,7 @@
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
     	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-    	<p> <a href="../mainpage/index.html?logout='1'" style="color: red;">logout</a> </p>
+    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
     <?php endif ?>
 </div>
 		
