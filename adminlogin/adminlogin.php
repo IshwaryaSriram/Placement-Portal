cd45c516aada6a3b77e375238aadaa7c552cef90
bhagavathir <bhagavathir9@gmail.com>
2021-05-26 21:34:46 +0530

    dgvgvd

--- adminlogin/adminlogin.php
+++ adminlogin/adminlogin.php
@@ -4,8 +4,8 @@
     <title>Admin Login</title>
     <?php include 'links.php' ?>
     <!-- <link rel="stylesheet" type="text/css" href="style.css"><php? echo time() ?> -->
-    <link rel="stylesheet" type="text/css" href="styl.css"><php? echo time() ?>
-
+    <link rel="stylesheet" type="text/css" href="styles.css"><php? echo time() ?>
+    
 </head>
 <body>
 <header>
