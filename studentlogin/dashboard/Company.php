04d4dda9af06e983816cec8de5b0878715fd0a2a
kavyasridhar1501 <kavyasridhar1501@gmail.com>
2021-05-27 10:06:52 +0400

    jdbds

--- studentlogin/dashboard/Company.php
+++ studentlogin/dashboard/Company.php
@@ -1,4 +1,46 @@
-<div id="Companies" class="portion" style="display:none">
-                <h2>Comapnies</h2>
-                <p>Company section</p>
-            </div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>COMPANIES</title>
+    <style>
+        h2{
+            color: white;
+            font-size: 2rem;
+            text-align: left;
+            font-family: 'Anton', sans-serif;
+            margin-left:1.5rem;
+        }
+        .options{
+            width: 50%;
+            margin-left:1.5rem;
+            margin-top:1.5rem;
+            line-height: 80px;
+            font-size: 1.4rem;
+            background: linear-gradient(45deg, #c3073f, #950740);
+            text-align: center;  
+            border-radius: 5px;         
+        }
+        a{
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div id="Companies" class="portion" style="display:none">
+        <div ><h2>Companies</h2></div>
+        <div class="options">
+        <a href="#">APPLY FOR A JOB</a>
+        </div>
+
+        <div class="options">
+        <a href="#">JOB VACCANCIES</a>
+        </div>
+
+        <div class="options">
+        <a href="#">ALUMINI REVIEWS</a>
+        </div>
+    </div>
+
+    
+</body>
+</html>
