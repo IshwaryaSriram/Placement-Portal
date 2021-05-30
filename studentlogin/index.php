424c5b1fbdfa79a4796f293ebd67c2a6e8b8b457
kavyasridhar1501 <kavyasridhar1501@gmail.com>
2021-05-30 21:56:45 +0400

    jkn

--- studentlogin/index.php
+++ studentlogin/index.php
@@ -11,24 +11,3 @@
   	header("location: ../mainpage/index.html");
   }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Student Home Page</title>
-	<link rel="stylesheet" type="text/css" href="styl.css">
-	<?php include 'links.php'?>
-</head>
-<body style="background-color: black;">
-	<!--styling using bootstrap classes -->
-    <div class="container center-div shadow ">
-        <div class="heading text-center text-white mb-5">Student HOME Page</div>
-           	<!-- logged in user information -->
-			<div style="text-align: center;">
-				<?php  if (isset($_SESSION['username'])) : ?>
-					<p>Welcome <strong><?php echo $_SESSION['username']."<br>"; ?></strong></p>
-					<p><a href="index.php?logout='1'" style="color:red;">Logout</a> </p>
-				<?php endif ?>
-			</div>	
-    </div>
-</body>
-</html>
